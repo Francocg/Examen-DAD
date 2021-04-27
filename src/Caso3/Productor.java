6f6b89a0a639db8761c289bb0cdc073ae80871d6
francocg <francocabanillas@upeu.edu.pe>
2021-04-27 16:48:42 -0500

    error

--- src/Caso3/Productor.java
+++ src/Caso3/Productor.java
@@ -7,9 +7,9 @@ public class Productor implements Runnable{
       }
       public void run() {
          while (true){
-                 int num=Utilidades.numAzar(10);
+                 int num=Utilidades.numAzar(10);//Aqui muestra un error que no hay solucion
                  while (colaCompartida.encolar(num)==false){
-                         Utilidades.esperarTiempoAzar(3000);
+                         Utilidades.esperarTiempoAzar(3000);//Aqui igualmente se muestra un error 
                  } /*Fin del while*/
 
                  System.out.println("Productor encoló el numero:"+num);
